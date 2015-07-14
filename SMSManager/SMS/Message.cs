ee7874272db99f153367171a8a4e6766794bd901
dangminhlong <dangminhlong@gmail.com>
2015-07-14 10:55:28 +0700

    c

--- SMSManager/SMS/Message.cs
+++ SMSManager/SMS/Message.cs
@@ -8,7 +8,7 @@ namespace SMSManager
 {
     public class Message
     {
-        public string date { get; set; }
+        public DateTime date { get; set; }
         public string id { get; set; }
         public string message { get; set; }
         public string messageType { get; set; }
@@ -16,6 +16,6 @@ namespace SMSManager
         public string receiver { get; set; }
         public string sender { get; set; }
         public string threadId { get; set; }
-        public string read { get; set; }
+        public bool read { get; set; }
     }
 }

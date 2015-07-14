a616661da232133bfa39f4c77b3bb276301651da
dangminhlong <dangminhlong@gmail.com>
2015-07-14 10:49:49 +0700

    commit

---
+++ SMSManager/SMS/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSManager
+{
+    public class Message
+    {
+        public string date { get; set; }
+        public string id { get; set; }
+        public string message { get; set; }
+        public string messageType { get; set; }
+        public string number { get; set; }
+        public string receiver { get; set; }
+        public string sender { get; set; }
+        public string threadId { get; set; }
+        public string read { get; set; }
+    }
+}
